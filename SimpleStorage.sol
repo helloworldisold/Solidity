010bd888dd1cb3e6feda436bcf4423109682276d
helloworldisold <56797611+helloworldisold@users.noreply.github.com>
2024-09-18 19:07:27 +0930

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -6,7 +6,7 @@ contract SimpleStorage{
     bool hasFavouritenumber = false;
     uint256 public favnumber = 88;// favnumber initialsed to 0 if no value is given default intsize is 256 uint can take only positive whole numbers 
     string favNumberinText="eighty-eight";
-    address myaddy=0x213341e5E180Fa7B42aCe13DD7d7895855cdb78a;
+    //address myaddy=0x213341e5E180Fa7B42aCe13DD7d7895855cdb78a;
     bytes32 favbytes="catordogs";
 
     struct Person {
@@ -19,6 +19,8 @@ contract SimpleStorage{
 
     Person[] public listOfpeople; // this is a dynamic array
 
+    mapping (string => uint256) public nametofavnumber;
+
  // the argument in function shoud have a different name and not the same as declared earlier
     function store(uint256 _favnumber) public {
         favnumber=_favnumber;
@@ -30,7 +32,7 @@ contract SimpleStorage{
 
     function addPeople(string memory _name,uint256 _favouritenumber ) public {
         listOfpeople.push(Person(_name,_favouritenumber));
-
+        nametofavnumber[_name]= _favouritenumber;
 
         //the below is valid too
         // Person memory newPerson = Person (_name, _favouritenumber);
